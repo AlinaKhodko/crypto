220afa681cf76111c475c758e337063468f0d56c
AlinaKhodko <32495783+AlinaKhodko@users.noreply.github.com>
2025-08-23 23:25:17 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -9,11 +9,11 @@ now_utc = datetime.now(timezone.utc)
 one_hour_ago = now_utc - timedelta(hours=1)
 
 # Date window for loading OHLC and iterating
-ARG_START_DATE     = os.getenv("ARG_START_DATE", "2025-08-22 05:00:00")  # None for full history
-#ARG_START_DATE    = os.getenv("ARG_START_DATE", now_utc.strftime("%Y-%m-%d %H:%M:%S"))
+#ARG_START_DATE     = os.getenv("ARG_START_DATE", "2025-08-22 05:00:00")  # None for full history
+ARG_START_DATE    = os.getenv("ARG_START_DATE", now_utc.strftime("%Y-%m-%d %H:%M:%S"))
 START_DATE_UPLOAD  = os.getenv("START_DATE_UPLOAD", "2023-01-01 00:00:00")
-END_DATE           = os.getenv("END_DATE", "2025-08-22 10:00:00")
-#END_DATE          = os.getenv("END_DATE", now_utc.strftime("%Y-%m-%d %H:%M:%S"))
+#END_DATE           = os.getenv("END_DATE", "2025-08-22 10:00:00")
+END_DATE          = os.getenv("END_DATE", now_utc.strftime("%Y-%m-%d %H:%M:%S"))
 
 
 # DB
