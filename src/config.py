4dc63da7ab5454c5131609967d5481fe24fb102d
AlinaKhodko <32495783+AlinaKhodko@users.noreply.github.com>
2025-08-23 13:43:19 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 import os
 
 # Date window for loading OHLC and iterating
-ARG_START_DATE     = os.getenv("ARG_START_DATE", "2025-01-01 00:00:00")  # None for full history
+ARG_START_DATE     = os.getenv("ARG_START_DATE", "2025-08-01 00:00:00")  # None for full history
 START_DATE_UPLOAD  = os.getenv("START_DATE_UPLOAD", "2023-01-01 00:00:00")
-END_DATE           = os.getenv("END_DATE", "2025-08-22 16:00:00")
+END_DATE           = os.getenv("END_DATE", "2025-08-24 16:00:00")
 
 # DB
 PG_DSN = os.getenv("PG_DSN_CRYPTO") or os.getenv("PG_DSN")
